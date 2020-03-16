17f80060e7371e127c4b64286ba3ea8f45ddd49a
katiadobra <broun.kat@gmail.com>
2020-03-16 08:32:05 +0100

    181. Dispatching Actions for Creating & Updating

--- screens/user/EditProductSreen.js
+++ screens/user/EditProductSreen.js
@@ -8,15 +8,17 @@ import {
   Platform
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import * as productsActions from '../../store/actions/products';
 
 const EditProductScreen = props => {
   const prodId = props.navigation.getParam('productId');
   const editedProduct = useSelector(state =>
     state.products.userProducts.find(prod => prod.id === prodId)
   );
+  const dispatch = useDispatch();
 
   const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
   const [imageUrl, setImageUrl] = useState(
@@ -27,9 +29,20 @@ const EditProductScreen = props => {
     editedProduct ? editedProduct.description : ''
   );
 
-  const submitHandler = useCallback(() => {
-    console.log('Submitting!');
-  }, []);
+  const submitHandler = useCallback(
+    () => {
+      if (editedProduct) {
+        dispatch(
+          productsActions.updateProduct(prodId, title, description, imageUrl)
+        );
+      } else {
+        dispatch(
+          productsActions.createProduct(title, description, imageUrl, +price)
+        );
+      }
+    },
+    [dispatch, prodId, title, description, imageUrl, price]
+  );
 
   useEffect(
     () => {
